c180d5e59da28372da6a4d6300ba8209fccfd57d
alam2tg <aaronlamfinance@gmail.com>
2023-09-06 06:40:41 -0700

    Add typeDefs

--- server/schemas/typeDefs.js
+++
@@ -1,7 +0,0 @@
-// create type for User, Book, Auth Token, Query, Mutation
-
-const typeDefs = `
-
-
-
-`
---
+++ server/schemes/typeDefs.js
@@ -0,0 +1,40 @@
+// create type for User, Book, Auth Token, Query, Mutation
+// check instructions.md
+
+const typeDefs = `
+    type User {
+        _id: ID,
+        username: String,
+        email: String,
+        password: String,
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID,
+        authors: [String],
+        description: String,
+        bookId: String,
+        image: String,
+        link: String,
+        title: String
+    }
+
+    type Auth {
+        token: ID!,
+        user: User
+    }
+
+    type Query {
+        user(username: String!): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        saveBook(profileId: ID!, bookData: Book!): User
+        deleteBook(profileId: ID!, bookId: ID!): User
+        login(email: String!, password: String!): Auth
+    }
+`
+
+module.exports = typeDefs;
