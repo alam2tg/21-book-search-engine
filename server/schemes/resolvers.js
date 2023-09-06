eba45e70c1d52d9cd288a938fbf86600ffc9cc97
alam2tg <aaronlamfinance@gmail.com>
2023-09-06 06:43:43 -0700

    Add Query => User.findOne.populate('savedBooks')

--- server/schemes/resolvers.js
+++ server/schemes/resolvers.js
@@ -2,3 +2,20 @@
 // const resolvers Query, Mutation
 	//query user to find saved books
 	//mutation Create User/Book, deleteBook, update (login)
+
+
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+	Query: {
+		user: async (parent, { username }) => {
+			return user.findOne({ username }).populate('savedBooks');
+	  },
+	},
+
+	Mutations: {
+
+	},
+
+}
