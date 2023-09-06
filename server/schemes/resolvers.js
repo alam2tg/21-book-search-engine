656fad46f13c121e60a75fa32f9287412f9205a3
alam2tg <aaronlamfinance@gmail.com>
2023-09-06 06:46:44 -0700

    add login, finish resolvers

--- server/schemes/resolvers.js
+++ server/schemes/resolvers.js
@@ -1,40 +1,51 @@
 // require user model + utils/auth
 // const resolvers Query, Mutation
-	//query user to find saved books
-	//mutation Create User/Book, deleteBook, update (login)
+//query user to find saved books
+//mutation Create User/Book, deleteBook, update (login)
 
-
-const { User } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
 	Query: {
 		user: async (parent, { username }) => {
-			return user.findOne({ username }).populate('savedBooks');
-	  },
+			return user.findOne({ username }).populate("savedBooks");
+		},
 	},
 
 	Mutations: {
 		saveBook: async (parent, { profileId, bookData }) => {
 			return User.findOneAndUpdate(
-				 { _id: profileId },
-				 {
-					  $addToSet: { savedBooks: bookData },
-				 },
-				 {
-					  new: true,
-					  runValidators: true,
-				 }
+				{ _id: profileId },
+				{
+					$addToSet: { savedBooks: bookData },
+				},
+				{
+					new: true,
+					runValidators: true,
+				}
 			);
-	  },
-	  deleteBook: async (parent, { profileId, bookId }) => {
+		},
+		deleteBook: async (parent, { profileId, bookId }) => {
 			return User.findOneAndUpdate(
-				 { _id: profileId },
-				 {pull: { savedBooks: bookId } },
-				 {new: true}
-			)
-	  },
-
+				{ _id: profileId },
+				{ pull: { savedBooks: bookId } },
+				{ new: true }
+			);
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+			if (!user) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+			const correctPw = await user.isCorrectPassword(password);
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+			const token = signToken(user);
+			return { token, user };
+		},
 	},
+};
 
-}
+module.exports = resolvers;
